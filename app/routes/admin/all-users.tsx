49935632a02b18e366eb0f1032f00289f6a319f6
Alvaro Marenco <alvaromarencom@gmail.com>
2025-10-01 16:52:15 -0600

    Initial routing

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,10 @@
+
+const allUsers = () => {
+  return (
+    <div>
+        Users Table
+    </div>
+  )
+}
+
+export default allUsers
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,10 @@
+
+const dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard page</h1>
+    </div>
+  )
+}
+
+export default dashboard
